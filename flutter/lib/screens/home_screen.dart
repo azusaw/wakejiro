fa15de4ec49f68e29fd66ec1d34e43ace874a9d9
azusaw <azusa0304@gmail.com>
2021-05-01 18:42:59 +0900

    watanabeブランチ作成

---
+++ flutter/lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen'),
+      ),
+      body: Center(
+        child: Text('watanabe-dayo'),
+      ),
+    );
+  }
+}
