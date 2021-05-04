4f5f9558d8562fab4a7057916c97edc8062fbec4
hachiden8 <hachiden8@gmail.com>
2021-05-04 16:21:33 +0900

    メンバー選択作成２（途中）

--- flutter/lib/components/steps/event_info_step.dart
+++ flutter/lib/components/steps/event_info_step.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sample/models/member.dart';
 import 'package:intl/intl.dart';
@@ -9,49 +10,84 @@ class EventInfoStep extends StatefulWidget {
 
 class _EventInfoStepState extends State<EventInfoStep> {
   // サンプルデータ
-  var memberCheckedMap = {
+  var _memberCheckedMap = {
     new Member(name: "八田"): false,
     new Member(name: "渡邉"): false,
     new Member(name: "宮谷"): false,
     new Member(name: "半田"): false
   };
+  var _memberAddedList = [];
   var _date = new DateTime.now();
+  var _memberNameToAdded = '';
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        Row(
-          children: [
-            Text(dateFormat(_date)),
-            IconButton(
-                onPressed: () => selectDate(context),
-                icon: Icon(Icons.calendar_today))
-          ],
-        ),
-        TextFormField(
-          decoration: InputDecoration(labelText: 'イベント名'),
-        ),
-        Text('参加メンバー'),
-        ListView.builder(
-          itemCount: memberCheckedMap.length,
-          itemBuilder: (context, index) {
-            return CheckboxListTile(
-              title: Text(memberCheckedMap.entries.elementAt(index).key.name),
-              value: memberCheckedMap.entries.elementAt(index).value,
-              onChanged: (newValue) {
-                setState(() {
-                  // TODO
-                });
-              },
-              controlAffinity: ListTileControlAffinity.leading,
-            );
-          },
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-        )
-      ],
-    );
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: [
+              Text(dateFormat(_date)),
+              IconButton(
+                  onPressed: () => selectDate(context),
+                  icon: Icon(Icons.calendar_today))
+            ],
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'イベント名'),
+          ),
+          SizedBox(height: 30),
+          Text('参加メンバー', textAlign: TextAlign.start),
+          ListView.builder(
+            itemCount: _memberCheckedMap.length,
+            itemBuilder: (context, index) {
+              return CheckboxListTile(
+                title:
+                    Text(_memberCheckedMap.entries.elementAt(index).key.name),
+                value: _memberCheckedMap.entries.elementAt(index).value,
+                onChanged: (newValue) {
+                  setState(() {
+                    _memberCheckedMap.update(
+                        _memberCheckedMap.entries.elementAt(index).key,
+                        (value) => newValue);
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              );
+            },
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+          ),
+          SizedBox(height: 30),
+          Row(
+            children: [
+              Expanded(
+                  child: TextFormField(
+                      initialValue: _memberNameToAdded,
+                      decoration: InputDecoration(labelText: 'メンバー名'),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _memberNameToAdded = newValue;
+                        });
+                      },
+                      )),
+              ElevatedButton(
+                onPressed: () {
+                  if (_memberNameToAdded.trim().isNotEmpty) {
+                    _memberAddedList
+                        .add(new Member(name: _memberNameToAdded.trim()));
+                    setState(() => _memberNameToAdded = 'hoge');
+                  }
+                },
+                child: Icon(Icons.add),
+                style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.white,
+                  shape: const CircleBorder(),
+                ),
+              ),
+            ],
+          )
+        ]);
   }
 
   Future<Null> selectDate(BuildContext context) async {
