c2ca4946412faa2dd4d16fda6895a7e41c60757e
hachiden8 <hachiden8@gmail.com>
2021-06-06 13:47:00 +0900

    清算一覧の作成処理を追加

--- flutter/lib/models/billing_details.dart
+++ flutter/lib/models/billing_details.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample/models/member.dart';
 import 'package:flutter_sample/models/paid_category.dart';
 
 class BillingDetails {
   BillingDetails({
-    @required this.paidPersonName,
+    @required this.paidMember,
     @required this.paidCategory,
     @required this.amount,
   });
 
-  String paidPersonName;
+  Member paidMember;
   PaidCategory paidCategory;
   int amount;
 
   BillingDetails copyWith(
-          {String paidPersonName, PaidCategory paidCategory, int amount}) =>
+          {Member paidMember, PaidCategory paidCategory, int amount}) =>
       BillingDetails(
-          paidPersonName: paidPersonName ?? this.paidPersonName,
+          paidMember: paidMember ?? this.paidMember,
           paidCategory: paidCategory ?? this.paidCategory,
           amount: amount ?? this.amount);
 }
